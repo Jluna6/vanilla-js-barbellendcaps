5b7e1132bac3dfe0210bbc5d81000265c111d1fb
Jluna6 <lunajonathan48@iclud.com>
2023-09-25 02:32:02 -0700

    User Profile Screen

--- frontend/README.md
+++ frontend/README.md
@@ -221,3 +221,11 @@ Vanilla JS barbellEndCaps
     3. after_render handle form submit
     4. create register request in frontend
     5. create register api in backend
+25. User Profile Screen
+    1. create ProfileScreen.js
+    2. add form elements
+    3. after_render handle form submit
+    4. create profile update request in frontend
+    5. create profile update api in backend
+    6. create isAuth in utils.js and use in update profile
+    7. implement sign out
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SigninScreen";
 import Header from "./components/Header";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const routes = {
   "/": HomeScreen,
@@ -14,7 +15,8 @@ const routes = {
   "/cart/:id": CartScreen,
   "/cart": CartScreen,
   "/signin": SigninScreen,
-  "/register": RegisterScreen
+  "/register": RegisterScreen,
+  "/profile": ProfileScreen
 };
 const router = async () => {
   showLoading();
@@ -30,7 +32,7 @@ const router = async () => {
   header.innerHTML = await Header.render();
   await Header.after_render();
   main.innerHTML = await screen.render();
-  await screen.after_render();
+  if (screen.after_render) await screen.after_render();
   hideLoading();
 };
 window.addEventListener("load", router);
